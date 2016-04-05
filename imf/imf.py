b28d8dd2cea8b9ee0d0ea06d51a2605f946bc1dd
aemerick <aemerick11@gmail.com>
2016-04-05 17:03:25 +0200

    sampler fixed . was broken

--- imf/imf.py
+++ imf/imf.py
@@ -1,11 +1,36 @@
+from scipy import integrate
+
 import numpy as np
 
+# just a bunch of IMF functions
 
-def salpeter(M, alpha=-2.35, xi_o=1.0):
-    return xi_o * M**(alpha)
+def kroupa(M, alpha_1 = 0.3, alpha_2 = 1.3, alpha_3 = 2.3, xi_o = 1.0):
+    M = np.asarray(M)
+    scalar_input = False
+    if M.ndim == 0:
+        M = M[None]
+        scalar_input = True
+
+    low_mass = M[ (M <= 0.08) ]
+    mid_mass = M[ (M  > 0.08)*(M <= 0.5) ]
+    salpeter = M[ (M  > 0.5 ) ]
+
+    dNdM = np.zeros(np.shape(M))
+
+    dNdM[low_mass] = M[low_mass]**(-alpha_1)
+    dNdM[mid_mass] = M[mid_mass]**(-alpha_2)
+    dNdM[salpeter] = M[salpeter]**(-alpha_3)
+
+    if scalar_input:
+        return np.squeeze(dNdM)
+    else:
+        return dNdM
+
+def salpeter(M, alpha = 2.35, xi_o=1.0):
+    return xi_o * M**(-alpha)
 
 
-def sample_IMF(N, IMF,  M_min = 0.1, M_max = 100.0, npoints = 1000, **kwargs):
+def sample_IMF(N, IMF,  M_min = 1.0, M_max = 100.0, npoints = 1000, **kwargs):
 
     # bin IMF in logspace
     dm = np.log10(M_max / M_min) / (1.0*(npoints - 1))
@@ -16,30 +41,20 @@ def sample_IMF(N, IMF,  M_min = 0.1, M_max = 100.0, npoints = 1000, **kwargs):
     i = np.arange(0, npoints)
     # cumulative probability density
     m        = 10.0**(m_o + i*dm)
-    total_fn = IMF(m) 
     
-
-    # determine mass sample points
-    M_sample = np.logspace(M_min, M_max, npoints)
- 
-    # sample the IMF 
-    IMF_vals = IMF(M_sample, **kwargs)
-    IMF_vals = IMF_vals / (1.0*np.sum(IMF_vals))
+    IMF_vals = IMF(m, **kwargs)
     IMF_vals = np.cumsum(IMF_vals)
-
-    # normalize to one
-
+    IMF_vals = IMF_vals / (IMF_vals[-1] * 1.0)
 
     random_numbers = np.random.rand(N)
     # now sample
 
-
-
     # do a bisect search for each number
     mass_sample = np.zeros(N)
-    bin_number = npoints / 2
-    width = npoints / 2 
     for i in np.arange(N):
+
+        bin_number = npoints / 2
+        width      = npoints / 2
 
         while ( width > 1):
             width = width / 2
@@ -56,7 +71,11 @@ def sample_IMF(N, IMF,  M_min = 0.1, M_max = 100.0, npoints = 1000, **kwargs):
     
     
      
+def integrate_imf(function, a, b):
 
+    # integrate a function from a to be
+
+    return integrate.quad(function, a, b)[0]
 
 
 def prob_SN1a(M_proj, t, t_form, dt,
@@ -72,3 +91,9 @@ def prob_SN1a(M_proj, t, t_form, dt,
     
     return norm_DTD * M_SF * t**(-beta) * dt
 
+def DTD(t, beta = 1.2, NSN = 0.00130):
+
+    Gyr = 3.1556E13 * 1.0E3
+    yr  = Gyr / 1.0E9
+
+    return NSN * (t / Gyr)**(-beta) 
